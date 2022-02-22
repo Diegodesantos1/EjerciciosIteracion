b5621ad8222ad39378600710b3888f56d164e6af
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 13:21:15 +0000

    Creada la variable base

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,10 @@
 # Ejercicio 7: Edición de un número entero
-import math
 def edicion(n):
   print("¿Qué número quieres editar?")
   numero = int(input())
+  print("¿Qué base quieres usar?")
+  base = int(input())
+  if base > 36:
+    base = 10
+  else:
+    
