11c17bf011d27dda326fb1aa1b60de3b2361d0f7
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 15:09:29 +0000

    importado sys y optimizado el código excepto ordenar por una letra

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,23 +1,27 @@
-
-print("¿Qué palabra quieres introducir en el diccionario?")
-palabra=str(input())
+import sys
 lista = []
-lista.append(palabra)
 def añadir_palabra():
   print("¿Qué palabra quieres introducir en el diccionario?")
   palabra = str(input())
   lista.append(palabra)
   print(lista)
+  print("¿Quieres introducir más palabras? si o no")
+  eleccion1=str(input())
+  if eleccion1 == "si":
+    añadir_palabra()
+  if eleccion1 == "no":
+    eleccion_actual()
 def eliminar_palabra():
   print("¿Qué palabra quieres eliminar?")
   palabra_borrar= str(input())
   lista.remove(f"{palabra_borrar}")
   print(lista)
+  eleccion_actual()
 def ordenar_palabra():
   lista.sort()
   print(lista)
 def eleccion_actual():
-  print("¿Qué quieres hacer? añadir,eliminar u ordenar ")
+  print("¿Qué quieres hacer? añadir, eliminar,ordenar o terminar ")
   eleccion= str(input())
   if eleccion == "añadir":
     añadir_palabra()
@@ -25,3 +29,6 @@ def eleccion_actual():
     eliminar_palabra()
   if eleccion == "ordenar":
     ordenar_palabra()
+  if eleccion == "terminar":
+    sys.exit
+añadir_palabra()
