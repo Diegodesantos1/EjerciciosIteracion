d61be8313480af48b3a9614e0644c7a80df97f55
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 09:32:03 +0000

    Creada la forma de sumas y restas y la selección de ejercicio en función de si quieres hacer el 11.1 o el 11.2

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,14 +1,32 @@
 print("¿Qué forma quieres ejecutar? 1(Euclides) o 2 (sumas y restas)")
 eleccion=int(input())
+print("Introduzca el primer número")
+n_1=int(input())
+print("Introduzca el segundo número")
+n_2 = int(input())
 if eleccion == 1:
-  print("Introduzca el primer número")
-  n1=int(input())
-  print("Introduzca el segundo número")
-  n2 = int(input())
   def mcd(n1, n2):
       if n2 == 0:
           return n1
       return mcd (n2, n1 % n2)
-  resultado =mcd(n1,n2)
-  print(f"El mcd de {n1} y {n2} es {resultado}")
+  resultado =mcd(n_1,n_2)
+  print(f"El mcd de {n_1} y {n_2} es {resultado}")
 elif eleccion == 2:
+  def mcd(n_1, n_2): 
+    if (n_1 == 0): 
+        return n_2 
+    if (n_2 == 0): 
+        return n_1 
+    if (n_1 == n_2): 
+        return n_1 
+    if (n_1 > n_2): 
+        return mcd(n_1-n_2, n_2) 
+    return mcd(n_1, n_2-n_1)
+  if(mcd(n_1, n_2)):
+    resultado = mcd(n_1, n_2)
+    print(f"El máximo común divisor de {n_1} y {n_2} es {resultado}") 
+else: 
+    print('No existe mcd') 
+
+  
+  
