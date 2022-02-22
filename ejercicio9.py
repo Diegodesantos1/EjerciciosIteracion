4b7361a9729da73469f43985402feebcee69dbf4
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 15:03:12 +0000

    cambios en las funciones

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,20 +1,27 @@
-from tabulate import tabulate
+
+print("¿Qué palabra quieres introducir en el diccionario?")
+palabra=str(input())
+lista = []
+lista.append(palabra)
 def añadir_palabra():
   print("¿Qué palabra quieres introducir en el diccionario?")
-  palabra=str(input())
-def tabla():
-    table = [['Diccionario'],[palabra]]
-    print(tabulate(table, headers='firstrow', tablefmt='grid'))
-tabla()
+  palabra = str(input())
+  lista.append(palabra)
+  print(lista)
 def eliminar_palabra():
   print("¿Qué palabra quieres eliminar?")
   palabra_borrar= str(input())
-  table.remove(f"{palabra_borrar}")
-eliminar_palabra()
-def ordenar_palabras():
-
-print("¿Qué quieres hacer? 1 añadir palabra 2 eliminar palabra 3 ordenar palabras")
-eleccion= int(input())
-if eleccion == 1:
-  def añadir_palabra()
-
+  lista.remove(f"{palabra_borrar}")
+  print(lista)
+def ordenar_palabra():
+  lista.sort()
+  print(lista)
+def eleccion_actual():
+  print("¿Qué quieres hacer? añadir,eliminar u ordenar ")
+  eleccion= str(input())
+  if eleccion == "añadir":
+    añadir_palabra()
+  if eleccion == "eliminar":
+    eliminar_palabra()
+  if eleccion == "ordenar":
+    ordenar_palabra()
