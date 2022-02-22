564eb7d61a768b7254a191b663efd4cacda45338
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 13:31:25 +0000

    aplicada recursividad

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,10 +1,10 @@
 # Ejercicio 7: Edición de un número entero
-def edicion(n):
-  resultado=[]
-  print("¿Qué número quieres editar?")
-  numero = int(input())
-  print("¿Qué base quieres usar?")
-  base = int(input())
+resultado=[]
+print("¿Qué número quieres editar?")
+numero = int(input())
+print("¿Qué base quieres usar?")
+base = int(input())
+def edicion(numero,base):
   if base > 36:
     base = 10
   elif base < 2:
@@ -15,3 +15,5 @@ def edicion(n):
       print(f"La solución es {resultado}")
     else:
       numero=numero//base
+      edicion(numero, base)
+edicion(numero, base)
