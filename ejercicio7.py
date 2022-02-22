83a5cdf9678921da5315ab28238ca74bb2b9c946
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 08:54:01 +0000

    eliminadas cosas innecesarias

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,11 +1,5 @@
 # Ejercicio 7: Edición de un número entero
 import math
-def edicion(numero):
+def edicion(n):
   print("¿Qué número quieres editar?")
   numero = int(input())
-  n =numero/base
-  if n == 0:
-    print(f"El {numero} es igual a elevado a 1")
-  else:
-    edicion(n)
-edicion(numero)
