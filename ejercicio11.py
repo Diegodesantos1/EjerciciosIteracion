a326986e77668d842f91c8896ba0e53f78d79ca4
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2022-02-22 09:05:30 +0000

    creada la selección de forma de resolver el ejercicio

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,10 +1,14 @@
-print("Introduzca el primer número")
-n1=int(input())
-print("Introduzca el segundo número")
-n2 = int(input())
-def mcd(n1, n2):
-    if n2 == 0:
-        return n1
-    return mcd (n2, n1 % n2)
-resultado =mcd(n1,n2)
-print(f"El mcd de {n1} y {n2} es {resultado}")
+print("¿Qué forma quieres ejecutar? 1(Euclides) o 2 (sumas y restas)")
+eleccion=int(input())
+if eleccion == 1:
+  print("Introduzca el primer número")
+  n1=int(input())
+  print("Introduzca el segundo número")
+  n2 = int(input())
+  def mcd(n1, n2):
+      if n2 == 0:
+          return n1
+      return mcd (n2, n1 % n2)
+  resultado =mcd(n1,n2)
+  print(f"El mcd de {n1} y {n2} es {resultado}")
+elif eleccion == 2:
