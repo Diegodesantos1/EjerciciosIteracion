e643303c4b6e94018a373dbf61f2f93906bcada0
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-23 13:56:48 +0000

    apartado cuadrados perfectos

--- ejercicio12.py
+++ ejercicio12.py
@@ -4,3 +4,12 @@ import math
 # Fragmento el ejercicio en los dos apartados que se plantean.
 pregunta = int(input("""Si desea obtener todos los cuadrados perfectos de un intervalo entre 0 y un parámetro introducido por usted pulse 1,
 en cambio si desea realizar la raíz cuadrada de un número entero pulse 2: """))
+# Procedemos con la realización del ejercicio.
+if pregunta == 1:
+    def cuadradosperfectos(a, b):
+
+        if (a**2 <= b):
+            print(a**2)
+            cuadradosperfectos(a+1, b)
+    cuadradosperfectos(
+        0, int(input("Introduzca hasta que valor quiere que llegue su lista de cuadrados perfectos: ")))
