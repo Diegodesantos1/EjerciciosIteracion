32578b0a0c9e56008161d3e7673a12921f77c74b
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-02-23 13:47:32 +0000

    realizo el import básico y formulo una pregunta

---
+++ ejercicio12.py
@@ -0,0 +1,6 @@
+# Importo las librerías necesarias para la realización del ejercicio.
+import math
+
+# Fragmento el ejercicio en los dos apartados que se plantean.
+pregunta = int(input("""Si desea obtener todos los cuadrados perfectos de un intervalo entre 0 y un parámetro introducido por usted pulse 1,
+en cambio si desea realizar la raíz cuadrada de un número entero pulse 2: """))
